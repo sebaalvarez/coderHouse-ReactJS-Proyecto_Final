ce973e6ed9b113d0b62ad6e8b9edcaafab110145
sebaalvarez <seba.alvarez@gmail.com>
2024-05-02 19:10:00 -0300

    primer commit

---
+++ src/components/card.jsx
@@ -0,0 +1,21 @@
+import reactLogo from "../assets/react.svg";
+import "./card.css";
+
+const card = () => {
+  return (
+    <>
+      <div className="card">
+        <img className="img" src={reactLogo} alt="logo react" />
+        <h3 className="titulo">Titulo Card</h3>
+        <p className="desc">
+          Descripción de Card Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. Nobis consequuntur ullam inventore voluptas, in amet
+          cum. Reprehenderit quisquam doloremque cumque modi explicabo sunt iure
+          maxime provident? Illum consequatur consectetur dolore.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default card;
---
+++ src/components/titulo.jsx
@@ -0,0 +1,9 @@
+const titulo = () => {
+  return (
+    <>
+      <h1> Primer componente con React JS </h1>
+    </>
+  );
+};
+
+export default titulo;
